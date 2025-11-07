344e141b355e4f4023ef1fb8f54316fa41454092
Shahar Ezra <ezra.shahar.2004@gmail.com>
2025-11-07 17:30:00 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -3,3 +3,59 @@ def approximate_pi(n_terms):
   for i in range (n_terms):
       pi_value += ((-1)**i) / (2*i+1)
   return pi_value * 4
+
+
+n_terms = 10
+list_of_numbers = []
+sign = -1
+for i in range(n_terms):
+    sign *= -1
+    list_of_numbers.append(sign)
+print (list_of_numbers)
+
+
+
+n_terms = 10
+list_of_numbers = []
+numerator = -1
+denominator = -1
+for i in range(n_terms):
+    numerator *= -1
+    denominator += 2
+    leibniz = (numerator) / (denominator)
+    list_of_numbers.append(leibniz)
+print (list_of_numbers)
+
+
+
+n_terms = 10
+list_of_numbers = []
+numerator = -1
+denominator = -1
+pi_sum = 0
+for i in range(n_terms):
+    numerator *= -1
+    denominator += 2
+    leibniz = numerator / denominator
+    list_of_numbers.append(leibniz)
+    pi_sum += leibniz
+    
+print (pi_sum *4)
+
+
+
+
+
+def approximate_pi(n_terms):
+    n_terms = 10
+    list_of_numbers = []
+    numerator = -1
+    denominator = -1
+    pi_sum = 0
+    for i in range(n_terms):
+      numerator *= -1
+      denominator += 2
+      leibniz = numerator / denominator
+      list_of_numbers.append(leibniz)
+      pi_sum += leibniz
+    return (pi_sum *4)
